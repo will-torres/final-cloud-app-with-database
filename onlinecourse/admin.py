182bca488454804d1966f8a4f3152272413fc3f9
will-torres <will.torres.jr@gmail.com>
2022-04-11 13:37:22 +0000

    minor updates

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -4,36 +4,31 @@ from .models import Course, Lesson, Instructor, Learner, Question, Choice
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
 
-class QuestionInline(admin.StackedInline):
+class QuestionInLine(admin.StackedInline):
     model = Question
+    extra = 5
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInLine(admin.StackedInline):
     model = Choice
+    extra = 5
 
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
 
-
-# Register your models here.
 class CourseAdmin(admin.ModelAdmin):
     inlines = [LessonInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
 
-
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
-    inlines = [QuestionInline]
-    #inlines = [ChoiceInline]
 
 
-# <HINT> Register Question and Choice models here
-
+admin.site.register(Question)
+admin.site.register(Choice)
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
-admin.site.register(Question)
-admin.site.register(Choice)
